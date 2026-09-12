de016cec1dd452522d0824b9d2a7f75974fb7aa4
XianZheng Lin <2520505328@stu.gdpu.edu.cn>
2026-09-12 19:29:51 +0800

    Add files via upload

---
+++ lesson12_条件运算符/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace lesson12_条件运算符
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 知识点一 条件运算符
+            Console.WriteLine("条件运算符");
+            //用于比较两个变量或常量
+            //是否大于>
+            //是否小于<
+            //是否等于==
+            //是否不等于!=
+            //是否大于等于>=
+            //是否小于等于<=
+
+            //条件运算符 一定存在左右两边的内容
+            //左边内容 条件运算符 右边内容
+            int a = 5;
+            int b=10;
+            //条件运算符 不能直接这样使用
+            //单纯比较不用结果 那么对于我们来说 没有任何意义
+            //比较的结果 返回的是 一个bool 类型的值 
+            //true和false 如果比较的条件满足 那就返回true 不满足 就返回false
+            //先算右边 再赋值给左边
+            bool result = a > b;
+            Console.WriteLine(result);
+
+            result = a < b;
+            Console.WriteLine(result);
+            #endregion
+
+            #region 知识点二 各种应用写法
+            //变量和变量比较
+            a = 5;
+            b = 10;
+            result= a < b;
+            //变量和数值(常量)比较
+            result = a > 3;
+            //数值和数值比较
+            result = 3>2;
+            //计算结果比较
+            //条件运算符 优先级 低于算术运算符
+            //7>6
+            //先计算 再比较
+            result = a + 2 > a - 2 + 3;
+            #endregion
+
+            #region 知识点三 不能进行范围比较
+
+            a = 5;
+            //判断是否在两个值之间
+            //1<a<6
+            //在C#不能这样写
+            //要判断 一个变量是否在 两个数之间 要结合 逻辑运算符的知识点
+
+            #endregion
+
+            #region 知识点四 不同类型之间的比较
+
+            //不同数值类型之间 可以随意进行条件运算符比较
+            int i = 2;
+            float f = 1.2f;
+            uint ui = 222;
+            result = i > f;
+            result i = ui > i;
+
+            //特殊类型 char string bool 只能 同 类型进行 == 和 != 比较
+            string str = "123";
+            char c = 's';
+            bool bo = true;
+            result = str == "234";
+            result = bo != true;
+
+            //char 不仅可以和自己类型进行==!= 还可以和数值类型进行比较
+            //还可以和 字符类型进行大小比较
+
+            result = c > 123;
+            result = c > '2';
+
+            #endregion
+        }
+    }
+}
---
+++ lesson13_逻辑运算符/Program.cs
@@ -0,0 +1,100 @@
+﻿namespace lesson13_逻辑运算符
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("逻辑运算符");
+            //对bool类型 进行逻辑运算
+
+            #region 知识点一 逻辑与
+            //符号 &&
+            //规则 对两个bool值进行逻辑运算 有假则假 同真则真
+
+            bool result = true && false ;
+            Console.WriteLine(result);//false
+            result = true && true&&true&&true;
+            Console.WriteLine(result);//true
+
+            //bool 相关的类型bool变量 条件运算符
+            //逻辑运算符优先级 低于 条件运算符 算术运算符
+            //false&&false&&true
+            result = 3 > 4 && 2 > 3&&7==7;
+            Console.WriteLine(result);
+
+            int i = 3;
+            //
+            result = i > 1 && i< 4;
+            Console.WriteLine(result);
+
+            //多个逻辑与 组合运用
+            //在没有括号的情况下 从左到右 依次看即可
+            //有括号 先看括号内
+
+            #endregion
+
+            #region 知识点二 逻辑或
+            //符号 ||
+            //规则 对两个bool值进行 逻辑运算 有真则真 同假则假
+            result = true || false || false || false ;
+            Console.WriteLine(result);
+            result = false||false || false ;
+            Console.WriteLine(result);
+            //其他同理
+            #endregion
+
+            #region 知识点三 逻辑非
+            //符号 ！
+            //规则 对一个bool值进行取反 真变假 假变真
+
+            result = !true;
+            Console.WriteLine(result);
+            result = !false;
+            Console.WriteLine(result);
+
+            result = !!true;
+            Console.WriteLine(result);
+            //逻辑非的 优先级 较高 要加括号
+            result = !(3>2);
+            Console.WriteLine(result);
+            #endregion
+
+            #region 知识点四 混合使用优先级问题
+            //规则 ！优先级最高 &&优先级高于||
+            //逻辑运算符优先级 低于 算术运算符 条件运算符（逻辑非除外）
+
+            bool gameOver = false;
+            int hp = 10;
+            bool isDead = false;
+            bool isMustOver = true;
+            //false||false&&true||true
+            result = gameOver || hp < 0 && !isDead || isMustOver;
+            Console.WriteLine(result);
+            #endregion
+
+            #region 知识点五 逻辑运算符短路规则
+
+            int i2 = 1;
+            //只要 逻辑与或者逻辑或 左边满足了条件
+            //只要 满足条件 右边的内容 对于我们来说 已经不重要
+
+            //逻辑或 有真为真 那左边只要为真了 右边就不重要了
+            //true||i2++>1;抛弃后面不去计算
+            result = i2 > 0 || ++i2 >= 1;
+            Console.WriteLine(i2);//答案是1 不是2
+            Console.WriteLine(result);
+
+            //逻辑与 有假为假 那左边只要为假了 右边就不重要了
+            //false&&i2++>1;抛弃后面不去计算
+            result = i2 < 0 && i2++ > 1;
+            Console.WriteLine(i2);//答案是1 不是2
+            Console.WriteLine(result);
+
+            result = i2++ > 1 && i2 < 0;
+            Console.WriteLine(i2);//答案是2
+            Console.WriteLine(result);
+            #endregion
+
+        }
+    }
+}
