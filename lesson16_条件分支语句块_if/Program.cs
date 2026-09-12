adff0a4fc60b62e0831562cdc5ebc8501c18dfd6
XianZheng Lin <2520505328@stu.gdpu.edu.cn>
2026-09-12 19:38:53 +0800

    Add files via upload

---
+++ lesson16_条件分支语句块_if/Program.cs
@@ -0,0 +1,96 @@
+﻿namespace lesson16_条件分支语句块_if
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("if语句");
+
+            #region 知识点一 作用
+            //让顺序执行的代码 产生分支
+            //if语句是第一个 可以让我们的程序 产生逻辑变化的 语句
+            #endregion
+
+            #region 知识点二 if语句
+            // 作用:满足条件时 多执行一些代码
+            //语法:
+            // if(boo1类型值)//boo1类型相关:boo1变量 条件运算符表达式 逻辑运算符表达式
+            //{
+            //   满足条件要执行的代码 写在if代码块中;
+            //}
+            // 注意:
+            // 1.if语句的语法部分，不需要写分号
+            // 2.if语句可以嵌套使用
+            if (3 > 5)
+            {
+                Console.WriteLine("这个是if语句");
+            }
+
+            int rightpassWord = (666);
+            string rightname = ("linxianzheng");
+            //嵌套使用
+            Console.WriteLine("请用户输入用户名");
+            string name = Console.ReadLine();
+
+
+            if (name == rightname)
+            {
+                Console.WriteLine("用户名输入正确");
+                Console.WriteLine("请用户输入密码");
+                try
+                {
+                    string passWord_ = Console.ReadLine();
+                    int passWord = int.Parse(passWord_);
+
+                    if (passWord == rightpassWord)
+                    {
+                        Console.WriteLine("密码输入正确");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("输入内容不合法");
+                }
+            }
+            #endregion
+
+            #region 知识点三 if  else语句
+            //作用:产生两条分支 十字路 满足条件做什么 不满足条件做什么
+            //语法：
+            //if( bool 类型值)
+            //{
+            //  满足条件执行的代码
+            //}
+            //else
+            //{
+            //  不满足条件执行的代码
+            //}
+            //注意：
+            //.if...else 语句 语法部分 不需要写分号
+            ///if...else 语句 可以嵌套
+            #endregion
+
+            #region 知识点四 if…else if…else语句
+            //作用：产生n条分支 多条道路选择 最先满足其中的一个条件 就做哪一个
+            //if( bool 类型值)
+            //{
+            //  满足条件执行的代码
+            //}
+            //else if（bool类型值）
+            //{
+            //  满足条件执行的代码
+            //}
+            //...中间可以有n个 else if语句代码块
+            //else
+            //{
+            //  不满足条件执行的代码
+            //}
+            // 注意:
+            //1.和前面两个是一样的 不需要写分号
+            //2.可以嵌套
+            //3.else  可以看成是补充 可以省略
+            //4.注意 条件判断 从上到下执行 满足了第一个后 之后的都不会执行了
+            #endregion
+        }
+    }
+}
---
+++ lesson17_条件分支语句_switch语句/Program.cs
@@ -0,0 +1,93 @@
+﻿namespace lesson17_条件分支语句_switch语句
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("switch语句");
+            #region 知识点一 作用
+            //让顺序执行的代码 产生分支
+            #endregion
+
+            #region 知识点二 基本语法
+            //case和break之间可以写n句语句
+            // 并且可以嵌套使用 也可以嵌套if等
+            //switch (变量)
+            //{
+            //    变量 == 常量 执行 case和 break之间的代码
+            //    case 常量:
+            //        满足条件执行的代码逻辑;
+            //        break:
+            //    case 常量:
+            //        满足条件执行的代码逻辑;
+            //        break;
+            //    case 可以有无数个
+            //    default:
+            //        如果上面case的条件都不满足 就会执行 default中的代码
+            //    break;
+            //}
+            // 注意: 常量!! 只能写一个值 不能去写一个范围 不能写条件运算符啊 逻辑运算符
+            //switch 只判断变量是否等于某一个固定值!!
+            //这个一般适配和枚举使用
+            int a = 3;
+            switch (a)
+            {
+                //条件一定是一个常量 
+                case 1:
+                    Console.WriteLine("a=1");
+                    break;
+                case 2:
+                    Console.WriteLine("a=2");
+                    break;
+                case 3:
+                    Console.WriteLine("a=3");
+                    break;
+                    default:
+                    Console.WriteLine("什么条件都不满足");
+                    break;
+            }
+            #endregion
+
+            #region 知识点三 dafault可省略
+            //像else一样
+            string str = "123";
+            switch (str)
+            {
+              
+            }
+            #endregion
+
+            #region 知识点四 可以自定义常量
+            char c = 'A';
+            const char c2 = 'a';//声明常量
+            switch (c)
+            {
+                case c2:
+                    Console.WriteLine("c=c2");
+                    break;
+                default:
+                    break;
+            }
+            #endregion
+
+            #region 知识点五 贯穿
+            // 作用:满足某些条件时 做的事情是一样的 就可以使用贯穿
+            int aa = 1;
+            switch (aa)
+            {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                    Console.WriteLine("是个数字");
+                   
+                    break;
+                default:
+                    Console.WriteLine("不是个数字");
+                    break;
+            }
+            #endregion
+
+        }
+    }
+}
